ff99c3dca61d93abd00c2df5fe7a656a58ea3442
dodgert87 <abdenoure520@gmail.com>
2024-10-16 21:32:14 +0300

    Polymorphism Principle

---
+++ src/main/java/org/example/Task/TaskTag.java
@@ -0,0 +1,13 @@
+package org.example.Task;
+
+public abstract class TaskTag {
+    
+    public class CriticalTask extends TaskTag {
+        // Specific implementation for critical tasks
+    }
+
+    public class NormalTask extends TaskTag {
+        // Specific implementation for normal tasks
+    }
+}
+
