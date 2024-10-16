fee14efa23c57010f7cf79ffe100e4892de127e0
dodgert87 <abdenoure520@gmail.com>
2024-10-16 22:12:51 +0300

    Protected Variations Principle

--- src/main/java/org/example/DataAccess/DatabaseDataAccess.java
+++ src/main/java/org/example/DataAccess/DatabaseDataAccess.java
@@ -1,5 +1,17 @@
 package org.example.DataAccess;
 
+import java.util.ArrayList;
+
 public class DatabaseDataAccess implements DataAccess {
     // Implementation using a database
+    @Override
+    public void saveProjects(List<Project> projects) {
+        // Save projects to database
+    }
+
+    @Override
+    public List<Project> loadProjects() {
+        // Load projects from database
+        return new ArrayList<>();
+    }
 }
--- src/main/java/org/example/DataAccess/FileDataAccess.java
+++ src/main/java/org/example/DataAccess/FileDataAccess.java
@@ -1,5 +1,17 @@
 package org.example.DataAccess;
 
+import java.util.ArrayList;
+
 public class FileDataAccess implements DataAccess {
     // Implementation using file system
+    @Override
+    public void saveProjects(List<Project> projects) {
+        // Save projects to file system
+    }
+
+    @Override
+    public List<Project> loadProjects() {
+        // Load projects from file system
+        return new ArrayList<>();
+    }
 }
